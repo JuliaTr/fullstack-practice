98ef28226ae0bfabc01276cac8fd67f3856b89a1
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-06-18 14:18:04 -0500

    Add ex4.js

---
+++ javaScriptAdvanced/constructors/constructorQuestions/ex4.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function User(name) {
+  this.name = name;
+}
+
+// #1
+User.prototype.test = function() {
+  console.log(111);
+};
+
+const yuliia = new User('Yuliia');
+// yuliia -> User.prototype #1
+
+yuliia.test(); // 111
+
+// #1, but `test` is different
+User.prototype.test = function() {
+  console.log(222);
+};
+
+yuliia.test() // 222
