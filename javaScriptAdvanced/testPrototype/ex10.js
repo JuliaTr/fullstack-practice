034a90cea8f161a2af472c4b704668ad979535db
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-06-19 15:10:25 -0500

    Add comments

--- javaScriptAdvanced/testPrototype/ex10.js
+++ javaScriptAdvanced/testPrototype/ex10.js
@@ -1,7 +1,14 @@
 const child = { name: 'Yuliia' };
 child.__proto__ = null;
+// The connection with basic methods including `hasOwnProperty`.
+// `__proto__` lives in `Object.prototype`. 
+// When `child.__proto__` is `null`, object has lost 
+// access to `Object.prototype`.
 
 child.__proto__ = Object.prototype
+// It doesn't change internal prototype. It creates 
+// simple text property with the same name inside object
+// Real prototype remains `null`.
 
 console.log(child.hasOwnProperty('name'));
 // TypeError: child.hasOwnProperty is not a function
