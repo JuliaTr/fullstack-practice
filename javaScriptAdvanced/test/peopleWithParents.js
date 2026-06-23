1a92de1cd7a203866e154e896d526ece0d3cdd03
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-06-23 16:02:46 -0500

    Debugged

--- javaScriptAdvanced/test/peopleWithParents.js
+++ javaScriptAdvanced/test/peopleWithParents.js
@@ -1,23 +1,27 @@
 function getPeopleWithParents(people) {
-  return people.map((person) => {
-    const fatherFound = people.find((personSearch) => {
+  const copyPeople = people.map((person) => ({ ...person }));
+
+  copyPeople.forEach((person) => {
+    const fatherIndex = people.findIndex((personSearch) => {
       return personSearch.name === person.father;
     });
 
-    if (fatherFound) {
-      person.father = fatherFound;
+    if (fatherIndex >= 0) {
+      person.father = copyPeople[fatherIndex];
     }
+  });
 
-    const motherFound = people.find((personSearch) => {
+  copyPeople.forEach((person) => {
+    const motherIndex = people.findIndex((personSearch) => {
       return personSearch.name === person.mother;
     });
 
-    if (motherFound) {
-      person.father = fatherFound;
+    if (motherIndex >= 0) {
+      person.mother = copyPeople[motherIndex];
     }
-
-    return person;
   });
+
+  return copyPeople;
 }
 
 const people = [
@@ -63,3 +67,54 @@ console.log(peopleWithParents);
 //   // ...
 // ]
 
+console.log(people);
+/*
+[
+  {
+    name: 'Carolus Haverbeke',
+    father: 'Carel Haverbeke',
+    mother: 'Maria van Brussel',
+    sex: 'm',
+    born: 1832,
+    died: 1905
+  },
+  {
+    name: 'Emma de Milliano',
+    father: 'Carolus Haverbeke',
+    mother: 'Sophia van Damme',
+    sex: 'f',
+    born: 1876,
+    died: 1956
+  }
+]
+*/
+
+// Works as expected
+
+
+
+// Experiments:
+// doesn't update with object from RESULTING array
+function getPeopleWithParents(people) {
+  return people.map((person) => {
+    const copyPerson = { ...person };
+
+    const fatherIndex = people.findIndex((personSearch) => {
+      return personSearch.name === person.father;
+    });
+
+    if (fatherIndex >= 0) {
+      copyPerson.father = people[fatherIndex];
+    }
+
+    const motherIndex = people.findIndex((personSearch) => {
+      return personSearch.name === person.mother;
+    });
+
+    if (motherIndex >= 0) {
+      copyPerson.mother = people[motherIndex];
+    }
+
+    return copyPerson;
+  });
+}
