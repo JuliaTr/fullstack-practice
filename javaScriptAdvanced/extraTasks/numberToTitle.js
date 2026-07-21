fc6678ba38e4142933ffddcc9ea53dfeab03f1f6
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-07-21 15:35:18 -0500

    Add numberToTitle.js

--- javaScriptAdvanced/extraTasks/numberToTitle.js
+++ javaScriptAdvanced/extraTasks/numberToTitle.js
@@ -24,71 +24,65 @@ Example
 ...
 */
 
-const ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const ALPHA = 'ZABCDEFGHIJKLMNOPQRSTUVWXY';
+const ZERO_IDX = 0; // 'Z'
 const ALPHA_LENGTH = ALPHA.length;
 
 function calculateWholeNumber(num) {
   return Math.trunc(num / ALPHA_LENGTH);
 }
 
-function calculateRemainder(num) {
-  return num % ALPHA_LENGTH;
-}
-
 function updateString(str, num) {
-  return ALPHA[num - 1] + str;
+  return ALPHA[num] + str;
 }
 
 function numberToTitle(num) {
-  let wholeNum = calculateWholeNumber(num);
-  let remainder = calculateRemainder(num);
+  let wholeNum = num;
   let str = '';
 
-  // console.log(wholeNum)
-  // console.log(remainder)
+  while (wholeNum > 0) {
+    const remainder = wholeNum % ALPHA_LENGTH;
 
-  while (remainder > 0) {
-    // console.log('--------------')
+    // To exit the loop
+    if (remainder === 0) {
+      str = updateString(str, ZERO_IDX);
+      wholeNum = calculateWholeNumber(wholeNum) - 1;
+    }
 
-    str = updateString(str, remainder);
-
-    // console.log(str)
-
-    remainder = calculateRemainder(wholeNum);
-    wholeNum = calculateWholeNumber(wholeNum);
+    // To continue
+    if (remainder > 0) {
+      str = updateString(str, remainder);
+      wholeNum = calculateWholeNumber(wholeNum);
+    }
   }
-
-  // if (remainder === 0 && wholeNum > 0) {
-  //   console.log('print1')
-  //   console.log(str)
-  //   console.log(wholeNum)
-  //   str = updateString(str, wholeNum - 1);
-  // }
-
-  // str = remainder > 0
-  //   ? updateString(str, remainder)
-  //   : updateString(str, ALPHA_LENGTH);
 
   return str;
 }
 
 console.log(numberToTitle(1)); // 'A'
+console.log(numberToTitle(25)); // 'Y'
+
+console.log(numberToTitle(26)); // 'Z'
+console.log(numberToTitle(27)); // 'AA'
 console.log(numberToTitle(28)); // 'AB'
-console.log(numberToTitle(11111)); // 'PKI'
+console.log(numberToTitle(29)); // 'AC'
+console.log(numberToTitle(52)); // 'BZ'
+
+// `should return 'ZY' if num = 701`
+console.log(numberToTitle(701)); // 'ZY'
 
 console.log(numberToTitle(702)); // 'ZZ'
+console.log(numberToTitle(703)); // 'AAA'
 
-// `should return 'FZPH' if num = 123456`
+console.log(numberToTitle(11111)); // 'PKI'
+
+// `should retuerTorn 'FZPH' if num = 123456`
 console.log(numberToTitle(123456)); // 'FZPH'
 
 // `should return 'ZZZ' if num = 18278`
 console.log(numberToTitle(18278)); // 'ZZZ'
 
-// `should return 'ZY' if num = 701`
-console.log(numberToTitle(701)); // 'ZY'
-
-// `should return 'Z' if num = 26`
-console.log(numberToTitle(26)); // 'Z'
+// Works as expected
 
 
 
@@ -130,25 +124,29 @@ console.log(numberToTitle(11111)); // 'PKI'
 
 
 
-// function numberToTitle(num) {
-//   let str = '';
-//   const outer = Math.floor(num / ALPHA_LENGTH);
 
-//   for (let idx1 = 0; idx1 <= outer; idx1++) {
-//     str += ALPHA[idx1];
+const ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const ALPHA_LENGTH = ALPHA.length;
+
+function numberToTitle(num) {
+  let str = '';
+  const outer = Math.floor(num / ALPHA_LENGTH);
+
+  for (let idx1 = 0; idx1 <= outer; idx1++) {
+    str += ALPHA[idx1];
   
-//     if (idx1 > 0) {
-//       for (let idx2 = 0; idx2 <= ALPHA_LENGTH; idx2++) { 
-//         if (num <= ALPHA_LENGTH && idx2 + 1 === num) {
-//           console.log('print1')
-//           str += ALPHA[idx2];
-//         }
-//       }
-//     }
-//   }
+    if (idx1 > 0) {
+      for (let idx2 = 0; idx2 <= ALPHA_LENGTH; idx2++) { 
+        if (num <= ALPHA_LENGTH && idx2 + 1 === num) {
+          console.log('print1')
+          str += ALPHA[idx2];
+        }
+      }
+    }
+  }
 
-//   return str;
-// }
+  return str;
+}
 
-// console.log(numberToTitle(1)); // 'A'
-// console.log(numberToTitle(28)); // 'AB'
+console.log(numberToTitle(1)); // 'A'
+console.log(numberToTitle(28)); // 'AB'
