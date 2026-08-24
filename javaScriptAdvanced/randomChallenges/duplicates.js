e60f9eaacff37bac7f373d390a1cc603ab08c1e1
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-08-24 12:49:12 -0500

    Add option 3

--- javaScriptAdvanced/randomChallenges/duplicates.js
+++ javaScriptAdvanced/randomChallenges/duplicates.js
@@ -1,51 +1,91 @@
 // Дано const source = [1,2,3,5,1,5,9,1,2,8]. Створіть масив без дублікатів і масив тільки з дублікатами.
 
-// const source = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
+const source = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
 
-// // Option 1:
-// const unique = [...new Set(source)];
-// console.log(unique); // [ 1, 2, 3, 5, 9, 8 ]
+// Option 1:
+const unique = [...new Set(source)];
+console.log(unique); // [ 1, 2, 3, 5, 9, 8 ]
 
-// // a value in the set **may only occur once**. 
-// // [...value] returns an array 
+// a value in the set **may only occur once**. 
+// [...value] returns an array 
 
-// const counts = source.reduce((acc, v) => {
-//   acc[v] = (acc[v] || 0) + 1;
-//   return acc;
-// }, {});
-// console.log(counts); 
-// // { '1': 3, '2': 2, '3': 1, '5': 2, '8': 1, '9': 1 }
+const counts = source.reduce((acc, v) => {
+  acc[v] = (acc[v] || 0) + 1;
+  return acc;
+}, {});
+console.log(counts); 
+// { '1': 3, '2': 2, '3': 1, '5': 2, '8': 1, '9': 1 }
 
-// const gotKeys = Object.keys(counts);
-// console.log(gotKeys); 
-// // [ '1', '2', '3', '5', '8', '9' ]
+const gotKeys = Object.keys(counts);
+console.log(gotKeys); 
+// [ '1', '2', '3', '5', '8', '9' ]
 
-// const filtered = gotKeys.filter(k => counts[k] > 1);
-// console.log(filtered); // [ '1', '2', '5' ]
+const filtered = gotKeys.filter(k => counts[k] > 1);
+console.log(filtered); // [ '1', '2', '5' ]
 
-// const duplicates = filtered.map(Number);
-// console.log(duplicates); // [ 1, 2, 5 ]
+const duplicates = filtered.map(Number);
+console.log(duplicates); // [ 1, 2, 5 ]
 
 // complexity: O(n)
 
 
 
 
-// // Option 2:
-// const source = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
+// Option 2:
+const source = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
 
-// const unique = [...new Set(source)];
-// console.log(unique); // [ 1, 2, 3, 5, 9, 8 ]
+const unique = [...new Set(source)];
+console.log(unique); // [ 1, 2, 3, 5, 9, 8 ]
 
-// const duplicates = [...new Set(source.filter((el, i, arr) => arr.indexOf(el) !== i))];
-// console.log(duplicates); // [ 1, 5, 2 ]
+const duplicates = [...new Set(source.filter((el, i, arr) => arr.indexOf(el) !== i))];
+console.log(duplicates); // [ 1, 5, 2 ]
 
-// // complexity 0(n^2)
+// complexity 0(n^2)
 
 
 
 
 // Option 3:
+const source = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
+
+console.log(getDedupedAndDuplicates(source));
+
+function getDedupedAndDuplicates(array) {
+  const registry = new Set();
+  console.log(registry); // Set(0) {}
+
+  const duplicates = new Set();
+  console.log(duplicates); // Set(0) {}
+
+  for (const el of array) {
+    // Creates collection without duplicated values
+    if (!registry.has(el)) {
+      registry.add(el);
+      continue;
+      // Skiped duplicates.add(el);
+    }
+
+    duplicates.add(el);
+    console.log(duplicates);
+  }
+
+  return {
+    deduped: [...registry],
+    duplicates: [...duplicates],
+  };
+}
+
+/*
+Set(0) {}
+Set(0) {}
+Set(1) { 1 }
+Set(2) { 1, 5 }
+Set(2) { 1, 5 }
+Set(3) { 1, 5, 2 }
+{ deduped: [ 1, 2, 3, 5, 9, 8 ], duplicates: [ 1, 5, 2 ] }
+*/
+
+
 
 // Experiments:
 const source = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
@@ -60,6 +100,7 @@ function getDedupedAndDuplicates(array) {
   console.log(duplicates); // Set(0) {}
 
   for (const el of array) {
+    // Creates collection without duplicated values
     if (!registry.has(el)) {
       registry.add(el);
       continue;
