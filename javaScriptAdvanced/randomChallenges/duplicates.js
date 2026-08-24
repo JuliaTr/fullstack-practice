26dd25421db2b2cb4102193d67546abbce529519
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-08-24 12:07:02 -0500

    Add object

--- javaScriptAdvanced/randomChallenges/duplicates.js
+++ javaScriptAdvanced/randomChallenges/duplicates.js
@@ -9,4 +9,10 @@ console.log(unique); // [ 1, 2, 3, 5, 9, 8 ]
 // a value in the set **may only occur once**. 
 // [...value] returns an array 
 
-const counts = source
+const counts = source.reduce((acc, v) => {
+  acc[v] = (acc[v] || 0) + 1;
+  return acc;
+}, {});
+console.log(counts); 
+// { '1': 3, '2': 2, '3': 1, '5': 2, '8': 1, '9': 1 }
+
