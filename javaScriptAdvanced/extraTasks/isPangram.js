2afc38ae117d343d890ea1e5447b991748290b07
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-09-17 13:54:54 -0500

    Add optimized solution

--- javaScriptAdvanced/extraTasks/isPangram.js
+++ javaScriptAdvanced/extraTasks/isPangram.js
@@ -8,6 +8,36 @@ Returns true, if str is a pangram, otherwise — false.
 💡 A pangram is a sentence that includes every letter of the alphabet at least once, regardless of case, like 'The quick brown fox jumps over the lazy dog'.
 */
 
+// Optimized solution
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+
+function isPangram(str) {
+  const letters = new Set(str.toLowerCase());
+
+  for (const letter of ALPHABET) {
+    if (!letters.has(letter)) return false;
+  }
+
+  return true;
+}
+
+console.log(
+  isPangram('abcdefghijklmnopqrstuvwxyz')
+); // === true
+
+console.log(
+  isPangram('Sphinx of black quartz, judge my vow.')
+); // === true
+
+console.log(
+  isPangram('AbCdEfGhIjKlMzxWvUtSrQpOn')
+); // === false // 'y' is missing
+
+console.log(isPangram('Detect Pangram')); //=== false 
+
+
+
+
 // Solution
 const ALPHA = 'abcdefghijklmnopqrstuvwxyz';
 
