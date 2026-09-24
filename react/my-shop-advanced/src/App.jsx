9706e3f68c200302e24c9e38f51c1cf4ba04f7d3
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-09-24 10:25:37 -0500

    Add component Card

--- react/my-shop-advanced/src/App.jsx
+++ react/my-shop-advanced/src/App.jsx
@@ -1,3 +1,4 @@
+import { Card } from "./components/Card/Card";
 import "./App.scss";
 
 export const App = () => {
@@ -5,38 +6,27 @@ export const App = () => {
     <div className="App">
       <h1>My-Shop</h1>
 
-      <div className="Card">
-        <h2 className="Card__title">Pizza</h2>
-        <div className="Card__details">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+      <Card
+        title="Pizza"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
             saepe placeat? Eligendi aperiam natus cupiditate delectus autem
-            modi. Exercitationem, voluptatibus!
-          </p>
-        </div>
-      </div>
-
-      <div className="Card">
-        <h2 className="Card__title">Chocolate</h2>
-        <div className="Card__details">
-          <p>
-            Provident quos ut dignissimos dicta voluptatum itaque asperiores
+            modi. Exercitationem, voluptatibus!"
+        price={1}
+      />
+      <Card
+        title="Chocolate"
+        text="Provident quos ut dignissimos dicta voluptatum itaque asperiores
             commodi adipisci! Ipsa qui ut rem nulla. Minima possimus ad cum
-            voluptatibus!
-          </p>
-        </div>
-      </div>
-
-      <div className="Card">
-        <h2 className="Card__title">Banana</h2>
-        <div className="Card__details">
-          <p>
-            Accusantium hic voluptates laboriosam quia expedita adipisci labore!
+            voluptatibus!"
+        price={10.988765}
+      />
+      <Card
+        title="Banana"
+        text="Accusantium hic voluptates laboriosam quia expedita adipisci labore!
             Explicabo nulla dolor mollitia enim nobis nisi accusamus iusto
-            neque, architecto pariatur!
-          </p>
-        </div>
-      </div>
+            neque, architecto pariatur!"
+        price={1.8976}
+      />
     </div>
   );
 };
